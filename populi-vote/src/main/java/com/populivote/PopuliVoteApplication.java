51a46943500be2da15d5c66b06fc74e37fd96f43
igor.ognjanov <igor.ognjanov@sorsix.com>
2025-05-15 21:35:59 +0200

    initial

---
+++ populi-vote/src/main/java/com/populivote/PopuliVoteApplication.java
@@ -0,0 +1,12 @@
+package com.populivote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopuliVoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopuliVoteApplication.class, args);
+    }
+}
---
+++ populi-vote/src/test/java/com/populivote/PopuliVoteApplicationTests.java
@@ -0,0 +1,12 @@
+package com.populivote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopuliVoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
